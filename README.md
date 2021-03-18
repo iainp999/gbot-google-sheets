10daef90f8be52dc00b234cddf7a6fbb2d61132e
Iain Potter <iain.potter@acquia.com>
2021-03-18 09:27:38 +0000

    Adds ability to create and write to new Google sheet.

---
+++ README.md
@@ -0,0 +1,10 @@
+# GlitcherBot Google Sheets Subscriber
+
+Listens for glitcherbot crawling events, gathers data, and will write data to Google sheets at the end of the crawl.
+
+## Configuration
+
+You will need to create a new application via the Google developer's console and create some OAUTH2 credentials with 
+permissions to write to Google sheets.  The type should be 'Desktop App'.
+
+Once done, download your credentials.json file and place it in the GoogleSheets folder.
--- src/Subscriber/CrawlSubscriber.php
+++ src/Subscriber/CrawlSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace ScraperBot\GoogleSheets\Subscriber;
 
+use Google\Exception;
 use ScraperBot\Event\CrawlCompleteEvent;
 use ScraperBot\Event\CrawledEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -21,8 +22,53 @@ class CrawlSubscriber implements EventSubscriberInterface {
     }
 
     public function onCrawlComplete(CrawlCompleteEvent $event) {
-        // Load GSheets credentials
-        // Write Data
+        if (count($this->crawlData) < 1) {
+            return;
+        }
+
+        // Create the Google sheets client.
+        $client = $this->getClient();
+
+        // Create sheet
+        $service = new \Google_Service_Sheets($client);
+
+        // Extract headers from data.
+        $values[] = array_keys(current($this->crawlData));
+
+        // Remove the associative indexes from the captured entries.
+        foreach ($this->crawlData as $data) {
+            $entry = [];
+
+            foreach($data as $value) {
+                $entry[] = $value;
+            }
+
+            $values[] = $entry;
+        }
+
+        $spreadsheet = new \Google_Service_Sheets_Spreadsheet([
+            'properties' => [
+                'title' => 'GlitcherBot Data'
+            ]
+        ]);
+
+        // TODO: store ID on first run as config, read from YAML.
+        $spreadsheet = $service->spreadsheets->create($spreadsheet, [
+            'fields' => 'spreadsheetId'
+        ]);
+
+        printf("Spreadsheet ID: %s\n", $spreadsheet->spreadsheetId);
+
+        // Write Data.
+        $body = new \Google_Service_Sheets_ValueRange([
+            'values' => $values
+        ]);
+        $params = [
+            'valueInputOption' => 'RAW'
+        ];
+        $result = $service->spreadsheets_values->append($spreadsheet->spreadsheetId, 'Sheet1!A:E', $body, $params);
+
+        printf("%d cells appended.", $result->getUpdates()->getUpdatedCells());
     }
 
     public static function getSubscribedEvents() {
@@ -30,6 +76,62 @@ class CrawlSubscriber implements EventSubscriberInterface {
             CrawledEvent::NAME => 'onCrawled',
             CrawlCompleteEvent::NAME => 'onCrawlComplete'
         ];
+    }
+
+    /**
+     * Returns an authorized API client.
+     *
+     * @return \Google_Client the authorized client object
+     * @throws Exception
+     */
+    private function getClient() {
+        require __DIR__ . '/../../vendor/autoload.php';
+
+        $client = new \Google_Client();
+        $client->setApplicationName('Google Sheets API PHP Quickstart');
+        $client->setScopes(\Google_Service_Sheets::DRIVE);
+        $client->setAuthConfig(__DIR__ . '/../../credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = __DIR__ . '/../../token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
     }
 
 }
